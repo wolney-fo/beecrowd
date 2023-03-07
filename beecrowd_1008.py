b7dec304b0e2eb6eb1fe299d227754c0ab1f59ab
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-03-07 00:33:06 -0300

    problem 1008

---
+++ beecrowd_1008.py
@@ -0,0 +1,7 @@
+employee_number = int(input())
+worked_hours = int(input())
+hourly_wage = float(input())
+
+
+print('NUMBER = {}'.format(employee_number))
+print('SALARY = U$ {:.2f}'.format(worked_hours * hourly_wage))
